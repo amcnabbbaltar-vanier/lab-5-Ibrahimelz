4bb588479ea244b982c0af15cb35bfc3d3b9f7e0
Ibrahimcomputerscience <6257643@edu.vaniercollege.qc.ca>
2025-12-28 21:00:42 -0500

    submit

---
+++ Assets/Scripts/HitDetector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyController enemyController = other.GetComponent<EnemyController>();
+            if (enemyController != null)
+            {
+                enemyController.GotHit();
+            }
+        }
+
+    }
+}
